e32767022fc6c4a828d24cb39353b8cadf22a2a7
bankonly <47017349+bankonly@users.noreply.github.com>
2020-06-19 10:44:43 +0700

    Server

--- src/v1/server.js
+++
@@ -1,9 +0,0 @@
-import { createServer } from "http";
-import app from "./setup/Droppin";
-
-const port = process.env.PORT || 1234;
-
-const server = createServer(app);
-console.log(port);
-
-server.listen(port);
