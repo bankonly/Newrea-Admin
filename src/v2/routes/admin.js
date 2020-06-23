fdfca1baa89eed884a95a3f80baa5e79457d03d6
bankonly <for21bn01@gmail.com>
2020-06-23 03:03:32 -0700

    Add mdw

--- src/v2/routes/admin.js
+++ src/v2/routes/admin.js
@@ -1,13 +1,15 @@
 const { Router } = require("express");
 const router = Router();
 const AdminCtrl = require("../controllers/admin_controller");
+const { AccessPermission } = require("../middlewares/AccessPermission");
+const mdw = [AccessPermission];
 
 router
   .get("/admin/me", AdminCtrl.whoami)
   .get("/admin", AdminCtrl.getAdminList)
-  .get("/admin/:admin_id", AdminCtrl.getAdminById)
-  .delete("/admin/:admin_id", AdminCtrl.deleteAdmin)
-  .put("/admin/:admin_id", AdminCtrl.updateAdmin)
+  .get("/admin/:admin_id", mdw, AdminCtrl.getAdminById)
+  .delete("/admin/:admin_id", mdw, AdminCtrl.deleteAdmin)
+  .put("/admin/:admin_id", mdw, AdminCtrl.updateAdmin)
   .post("/admin/changePassword", AdminCtrl.changePassword);
 
 export default router;
--- src/v2/routes/banner.js
+++ src/v2/routes/banner.js
@@ -2,12 +2,12 @@ import { Router } from "express";
 const router = Router();
 const BannerCtrl = require("../controllers/banner_controller");
 const { AccessPermission } = require("../middlewares/AccessPermission");
-
+const mdw = [AccessPermission];
 router
   .get("/banner", BannerCtrl.getAllBanner)
   .get("/banner/:banner_id", BannerCtrl.getBanner)
-  .delete("/banner/:banner_id", [AccessPermission], BannerCtrl.deleteBanner)
-  .put("/banner/:banner_id", [AccessPermission], BannerCtrl.updateBanner)
-  .post("/banner", [AccessPermission], BannerCtrl.saveBanner);
+  .delete("/banner/:banner_id", mdw, BannerCtrl.deleteBanner)
+  .put("/banner/:banner_id", mdw, BannerCtrl.updateBanner)
+  .post("/banner", mdw, BannerCtrl.saveBanner);
 
 export default router;
