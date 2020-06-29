60c0f620644d30c7ef0f9de31f0e75ea15ba513d
bankonly <for21bn01@gmail.com>
2020-06-29 14:39:46 +0700

    add product option

--- src/v2/providers/recommend_item_provider.js
+++ src/v2/providers/recommend_item_provider.js
@@ -21,8 +21,14 @@ export function validate(obj, update = false) {
 
 export const default_populate = {
   path: "product_seller_id",
-  select: "_id name",
-  populate: {
-    path: "product_option_id",
-  },
+  select: "_id price",
+  populate: [
+    {
+      path: "product_option_id",
+    },
+    {
+      path:"product_master_id",
+      select:"_id name"
+    }
+  ],
 };
