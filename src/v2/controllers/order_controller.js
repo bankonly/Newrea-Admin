5930aa0803feb6e3e8865b9e425d6cee8f85ef91
droppin <droppin@droppins-MacBook-Pro.local>
2020-06-29 16:48:40 +0700

    fetch orders list

--- src/v2/controllers/order_controller.js
+++ src/v2/controllers/order_controller.js
@@ -2,26 +2,39 @@ import Res from "./response_controller";
 const config = require("./../configs/constant");
 
 const orderModel = require("../models/orderModel");
+const orderStatusModel = require("../models/orderStatusModel");
+const productItemModel = require("../models/Product_item");
 
 // get all order
 exports.getOrders = async (req, res) => {
   const response = new Res(res);
   try {
-    const foundOrder = await orderModel.findOne().populate([
+    const foundOrder = await productItemModel.find().populate([
       {
-        path: "cus_id",
+        path: "order_id",
+        populate: [
+          {
+            path: "cus_id",
+          },
+          {
+            path: "address_id",
+          },
+          {
+            path: "currency_id",
+          },
+          {
+            path: "delivery_fee_id",
+          },
+          {
+            path: "payment_method_id",
+          },
+        ],
       },
       {
-        path: "address_id",
+        path: "pd_item_status",
       },
       {
-        path: "currency_id",
-      },
-      {
-        path: "delivery_fee_id",
-      },
-      {
-        path: "payment_method_id",
+        path: "items.pd_seller_id",
       },
     ]);
     if (foundOrder.length > 0) {
