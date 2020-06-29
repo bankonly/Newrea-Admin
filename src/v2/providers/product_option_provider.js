09a632bed67b0150c6e860ec91069ddb5e9feff4
bankonly <for21bn01@gmail.com>
2020-06-29 14:27:01 +0700

    add product option

---
+++ src/v2/providers/product_option_provider.js
@@ -0,0 +1,9 @@
+const Res = require("../controllers/default_res_controller");
+const Product_option = require("../models/Product_option");
+const Helpers = require("../helpers/Global");
+
+// validate save data
+export function validate(obj, update = true) {
+  var error = {};
+  return error;
+}
