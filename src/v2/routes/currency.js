37b4f0a60617fe1a2167e0ab35c0500c235b2cc6
droppin <droppin@droppins-MacBook-Pro.local>
2020-06-29 13:06:33 +0700

    currency CRUD macky

--- src/v2/routes/currency.js
+++ src/v2/routes/currency.js
@@ -3,7 +3,27 @@ const express = require("express");
 const router = express.Router();
 
 const currencyController = require("./../controllers/currency_controller");
+const validator = require("./../middlewares/validations/currency_validator");
+const {
+  checkValidObjectId,
+  deleteValidator,
+} = require("./../middlewares/validations/commonValidator");
 
 router.get("/currency", currencyController.getCurrency);
+router.post(
+  "/currency",
+  [validator.createValidator],
+  currencyController.createCurrency
+);
+router.put(
+  "/currency/:id",
+  [checkValidObjectId, validator.updateValidator],
+  currencyController.actionCurrency
+);
+router.delete(
+  "/currency/:id",
+  [checkValidObjectId, deleteValidator],
+  currencyController.actionCurrency
+);
 
 module.exports = router;
