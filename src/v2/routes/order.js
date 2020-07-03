a811dfef0c6e10644361862c41988dfec35817da
bankonly <for21bn01@gmail.com>
2020-07-03 13:23:31 +0700

    ADD Middleware

--- src/v2/routes/order.js
+++ src/v2/routes/order.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const orderController = require("./../controllers/order_controller");
 const validator = require("./../middlewares/validations/order_validator");
+const { AccessPermission } = require("../middlewares/AccessPermission");
+const mdw = [AccessPermission];
 
-router.get("/order/allOrders", orderController.getOrders);
+router.get("/order/allOrders",mdw, orderController.getOrders);
 router.get("/order/asignedOrders", orderController.getAsignedOrders);
 router.post(
   "/order/asignToDriver",
